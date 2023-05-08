1767fc2bf5313770980956dc4af730353ed567c4
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-05-08 21:49:40 +0300

    feat: done copy directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 
+const filesPath = path.join(__dirname, 'files'),
+filesCopy = path.join(__dirname, 'files-copy');
+const copyDir = async () => { 
+    await fs.promises.mkdir(filesCopy, { recursive: true });
+    fs.readdir(filesPath, (err, files) => { 
+        files.forEach(file => {
+            fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+        });
+    });
+}
+copyDir();
